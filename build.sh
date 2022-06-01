283984d5a7bdb2a68d0a67bf85af425f4823c0b4
Carlos Camacho <carloscamachoucv@gmail.com>
2022-06-01 11:38:27 +0200

    test

--- build.sh
+++ build.sh
@@ -9,10 +9,12 @@ ls Dockerfile.* | xargs -L1 grep 'COPY --from=' >> Dockerfile
 # Export build artifact to local path
 DOCKER_BUILDKIT=1 docker build -o type=local,dest=$PWD -f Dockerfile .
 
-docker image list
+ls -ltah
+# docker image list
 
 # id=$(docker create image-name)
 # docker cp $id:path - > local-tar-file
 # docker rm -v $id
+
 # docker create --name suspect-container suspect-image:latest
 # docker export suspect-container > suspect-container.tar
